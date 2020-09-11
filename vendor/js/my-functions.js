bee7fa8853739d9face8cf0a603a8012c94ad9c8
MuzooraBarnabas <bmuzoora@gmail.com>
2020-09-11 14:24:43 +0300

    updated login form and app.css

--- vendor/js/my-functions.js
+++ vendor/js/my-functions.js
@@ -42,7 +42,7 @@ function get_bank_list_for_select() {
                                     <input type="hidden" name="bank_id" value="{{id}}">
                                     <input type="hidden" name="bank_name" value="{{title}}">
                                     <input type="hidden" name="bank_logo" value="${api_public}{{logo}}">
-                                        <div class="item-media"><img
+                                        <div class="item-media"><img class="image-block imaged"
                                                 src="${api_public}{{logo}}"
                                                 width="44" /></div>
                                         <div class="item-inner">
@@ -70,17 +70,15 @@ function get_bank_list_for_select() {
             app.dialog.create({
                 title: "CONNECTION ERROR",
                 text: 'Server Connection Failed',
-                buttons: [
-                    {
-                        text: 'Retry',
-                        onClick: () => {
-                            reload_app();
-                        }
+                buttons: [{
+                    text: 'Retry',
+                    onClick: () => {
+                        reload_app();
                     }
-                ],
+                }],
                 verticalButtons: false,
             }).open();
-            
+
         })
 
 
@@ -88,7 +86,7 @@ function get_bank_list_for_select() {
 }
 
 // reload app
-function reload_app(){
+function reload_app() {
     // show app preloader
     app.preloader.show("#6236FF");
     get_bank_list_for_select();
@@ -104,23 +102,103 @@ function confirm_bank(bank_id) {
         title: "<img src='" + bank_details.bank_logo + "' style='border-radius:100%; width=55px;'/><br/>" + bank_details.bank_name,
         text: 'Continue With Selected Bank?',
         buttons: [{
-                text: 'No',
+                text: '<i class="f7-icons">delete_left_fill</i>',
                 onClick: () => {
-                    console.log("Clicked No")
+
+                    app.notification.create({
+                        icon: '<i class="f7-icons">exclamationmark_bubble_fill</i>',
+                        title: 'Heads Up!',
+                        titleRightText: 'now',
+                        subtitle: '',
+                        text: 'Choose a bank to continue!',
+                        closeOnClick: true,
+                        closeTimeout: 3000,
+                    }).open();
+
                 },
                 close: true,
 
             },
             {
-                text: 'Continue <i class="fas fa-arrow-alt-circle-right"></i>',
+                text: '<b><i class="f7-icons">square_arrow_right_fill</i></b>',
                 onClick: () => {
-                    console.log("Clicked Continue")
+                    app.notification.close();
+                    app.preloader.show("#6236FF");
                     // open popup
                     var swipeToClosePopup = app.popup.create({
                         el: '.bank_pin',
                         swipeToClose: true,
-                      });
-                      swipeToClosePopup.open();
+                    });
+
+
+                    // show bank details data
+
+                    var template = ` <div class="col-12 mb-2">
+                    <div class="bill-box">
+                        <div class="img-wrapper">
+                            <img src="${bank_details.bank_logo}" alt="img"
+                                class="image-block imaged w100">
+                        </div>
+                        <div class="price">${bank_details.bank_name}</div>
+
+                        <p>
+                            Enter Phone Number And Bank Pin
+                        </p>
+                        <form style="margin-top:-20px;">
+
+                            <div class="row">
+                                <div class="col-20">
+                                    <i style="margin-top:25px;"
+                                        class="f7-icons">phone_circle_fill</i>
+                                </div>
+
+                                <div class="col-80">
+                                    <div class="form-group basic animated">
+                                        <div class="input-wrapper">
+                                            <input type="tel" class="form-control"
+                                                id="phone_number" name="phone_number"
+                                                placeholder="Phone Number">
+                                                <span class="input-clear-button"></span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div class="row">
+                                <div class="col-20">
+                                    <i style="margin-top:25px;"
+                                        class="f7-icons">lock_circle_fill</i>
+                                </div>
+
+                                <div class="col-80">
+                                    <div class="form-group basic animated">
+                                        <div class="input-wrapper">
+                                            <input type="password" class="form-control"
+                                                id="bank_pin" name="bank_pin"
+                                                placeholder="PIN">
+                                            <span class="input-clear-button"></span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </form>
+
+                        <button type="button" class="btn btn-primary btn-sm mr-1 mb-1">
+                            
+                            LOGIN <i class="f7-icons">square_arrow_right</i>
+                        </button>
+                    </div>
+                </div>`;
+
+
+                    $$('.show_bank_info').html(template);
+
+                    // show popup
+                    swipeToClosePopup.open();
+                    app.preloader.hide();
                 }
             }
         ],
